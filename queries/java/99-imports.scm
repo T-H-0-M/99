9b60f58313a12154af76ccd64f8a32fa030b4f46
T_H_0_M <thomasbandy23@gmail.com>
2026-01-07 16:43:14 +1100

    add support for java

---
+++ queries/java/99-imports.scm
@@ -0,0 +1,8 @@
+(import_declaration
+  (scoped_identifier) @import.name) @import.decl
+
+(import_declaration
+  "static"
+  (scoped_identifier) @import.name) @import.decl
+
+;; I couldn't get wildcard imports to work
